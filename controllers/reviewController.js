540fd6324ac5c0894b2f2e42729cd999cec8ed89
DESKTOP-OTFT78U\Ionut <enacheionut2399@gmail.com>
2023-07-02 09:04:52 -0700

    refactor again

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -24,7 +24,6 @@ const createReview = async (req, res) => {
     );
   }
 
-  req.body.user = req.user.userId;
   const review = await Review.create(req.body);
   res.status(StatusCodes.CREATED).json({ review });
 };
